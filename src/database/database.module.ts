6b971cbc9760d23def9b882567fcb4d1fdd97ab1
omidev <omarblanco54@gmail.com>
2023-05-10 20:12:25 -0400

    configure typeorm with postgreSQL

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,13 +1,28 @@
 import { Module, Global } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigType } from '@nestjs/config';
+
+import config from '../config';
 
 @Global()
 @Module({
-  providers: [
-    {
-      provide: 'API_KEY',
-      useValue: '123456',
-    },
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, host, dbName, password, port } = configService.postgres;
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username: user,
+          password,
+          database: dbName,
+        };
+      },
+    }),
   ],
-  exports: ['API_KEY'],
+  providers: [],
+  exports: [TypeOrmModule],
 })
 export class DatabaseModule {}
