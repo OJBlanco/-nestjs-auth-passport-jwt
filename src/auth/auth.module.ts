371395ebfb7a9456d3098ffa2001127577e27524
omiDev <omarblanco54@gmail.com>
2023-08-25 20:44:50 -0400

    add auth.service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
+import { UsersService } from '../users/services/users.service';
 
 @Module({
+  imports: [UsersService],
   providers: [AuthService],
 })
 export class AuthModule {}
