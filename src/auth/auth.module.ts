40ebca22b2a292ba9b9e7bddc42e7e50b08c94d7
omiDev <omarblanco54@gmail.com>
2023-08-25 21:16:17 -0400

    create login EP

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
 import { AuthService } from './services/auth.service';
-import { UsersService } from '../users/services/users.service';
 import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './controllers/auth.controller';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [UsersService, PassportModule],
+  imports: [UsersModule, PassportModule],
   providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
